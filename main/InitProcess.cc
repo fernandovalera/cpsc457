ba54ed66c7f5ee59dee7305ebe71678008df9da5
Fernando Valera <fernando.valera@ucalgary.ca>
2016-10-10 22:11:49 -0600

    finally applied patch 2

--- main/InitProcess.cc
+++ main/InitProcess.cc
@@ -19,8 +19,8 @@
 int InitProcess() {
 //  Process* p5 = knew<Process>();
 //  p5->exec("IsEven");
-  Process* p4 = knew<Process>();
-  p4->exec("myprogram");
+//  Process* p4 = knew<Process>();
+//  p4->exec("myprogram");
   Process* p0 = knew<Process>();
   p0->exec("systest");
 #if !TESTING_KEYCODE_LOOP
